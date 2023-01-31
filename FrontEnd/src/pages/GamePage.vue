7ce7dfc4dcbe7ce658b5a6d2ed404ebbd586e1c0
xlukacs <43961338+xlukacs@users.noreply.github.com>
2023-01-31 13:49:37 +0100

    Moving duck

--- FrontEnd/src/pages/GamePage.vue
+++ FrontEnd/src/pages/GamePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="gameArea"></div>
+  <div id="gameArea" v-on:click="movePlayer"></div>
 </template>
 
 <script>
@@ -14,92 +14,102 @@ import { DirectionalLight } from "three";
 
 import { defineComponent } from "vue";
 
+import { loadObject } from "assets/scripts/util";
+
+let scene, camera, renderer;
+
 export default defineComponent({
   name: "GamePage",
   setup() {
     return {};
   },
-  methods: {},
+  data() {
+    return {};
+  },
+  methods: {
+    movePlayer(ev) {
+      console.log(ev.clientX);
+      console.log(ev.clientY);
+
+      const screenMiddleX = window.innerWidth / 2;
+      const screenMiddleY = window.innerHeight / 2;
+
+      var moveByX = screenMiddleX - ev.clientX;
+      var moveByY = screenMiddleY - ev.clientY;
+
+      moveByX /= -10;
+      moveByY /= -10;
+
+      var object = scene.getObjectByName("rubberDucky", true);
+      object.position.set(
+        object.position.x + moveByX,
+        0,
+        object.position.z + moveByY
+      );
+
+      camera.position.set(
+        camera.position.x + moveByX,
+        camera.position.y,
+        camera.position.z + moveByY
+      );
+    },
+    init() {
+      //scene
+      scene = new THREE.Scene();
+
+      //camera
+      camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      camera.position.set(0, 25, 10);
+
+      //render pipeline
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.getElementById("gameArea").appendChild(renderer.domElement);
+
+      //basic light
+      const light = new DirectionalLight(0xffffff, 1);
+      light.position.set(0, 10, 0);
+      light.castShadow = true;
+      scene.add(light);
+    },
+  },
   mounted() {
-    // create scene, camera and renderer
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
+    this.init();
 
-    ///camera.position.y = 15;
-    //camera.position.x = 15;
-    //camera.position.z = -5;
-    camera.position.set(5, 5, 15);
-    const renderer = new THREE.WebGLRenderer();
-
-    // set renderer size
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // add renderer to mount point
-    document.getElementById("gameArea").appendChild(renderer.domElement);
-
-    const light = new DirectionalLight(0xffffff, 1);
-    light.position.set(0, 10, 0);
-    light.castShadow = true;
-
-    // Add the light to the scene
-    scene.add(light);
-
+    //testCube
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    // instantiate a loader
-    const loader = new OBJLoader();
-    const textureLoader = new TextureLoader();
-
-    // load a resource
-    loader.load(
-      // resource URL
-      "assets/models/rubberDuck.obj",
-      // called when resource is loaded
-      function (object) {
-        const texture = textureLoader.load(
-          "assets/models/rubberDuck.bmp",
-          () => {
-            // Create a MeshLambertMaterial with the texture
-            const material = new MeshLambertMaterial({
-              map: texture,
-            });
-
-            // Apply the material to the mesh
-            object.traverse((child) => {
-              if (child instanceof Mesh) {
-                child.material = material;
-              }
-            });
-
-            // Add the mesh to your scene
-            scene.add(object);
-            object.position.set(0, 0, 0);
-            object.scale.set(1, 1, 1);
-            //object.rotation.y += 3.14159;
-            //object.rotation.z += 3.14159;
-            object.rotation.x -= Math.PI / 2;
-            //camera.lookAt(object.position);
-          }
-        );
-      },
-      // called when loading is in progresses
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-      },
-      // called when loading has errors
-      function (error) {
-        console.log("An error happened");
-        console.error(error);
-      }
+    //test duck
+    loadObject(
+      scene,
+      camera,
+      "rubberDuck.obj",
+      "rubberDuck.bmp",
+      "rubberDucky"
     );
+
+    //test duck
+    loadObject(scene, camera, "cube.obj", "grass.bmp", "ground");
+
+    //access the objects in the scene
+    setTimeout(() => {
+      var object = scene.getObjectByName("rubberDucky", true);
+      object.position.set(0, 0, 0);
+      object.scale.set(1, 1, 1);
+      object.rotation.x -= Math.PI / 2;
+
+      object = scene.getObjectByName("ground", true);
+      object.position.set(0, 0, 0);
+      object.scale.set(100, 1, 100);
+    }, 1000);
 
     // render loop
     const animate = () => {
